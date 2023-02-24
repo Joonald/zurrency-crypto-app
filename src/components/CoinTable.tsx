ff200d323686a93ab3bed009c8b97f15296b5c3c
Jonny Nguyen <jonnypn9@gmail.com>
2023-02-23 20:43:36 -0800

    completed homepage on desktop and tablet design, and included loaders

--- src/components/CoinTable.tsx
+++ src/components/CoinTable.tsx
@@ -1,4 +1,4 @@
-import { Container, TableContainer, Typography, TableHead, TableRow, TableCell, Paper, Table, TableBody } from "@mui/material";
+import { Container, TableContainer, Typography, TableHead, TableRow, TableCell, Paper, Table, TableBody, Box, LinearProgress } from "@mui/material";
 import { useState, useEffect } from "react";
 import { CoinMarketList } from "../config/apiConfig";
 
@@ -24,6 +24,8 @@ interface ICoin {
     atl_date: string;
     roi?: unknown;
     last_updated: string;
+    price_change_percentage_7d_in_currency: number;
+    price_change_percentage_30d_in_currency: number;
   }
 
 
@@ -49,17 +51,19 @@ const CoinTable = ()  => {
   console.log(coinData);
 
     return (
-        <Container sx={{ textAlign: 'center', width: '97%', paddingRight: 0, paddingLeft: '1.75rem', marginRight: '.5rem' }}>
+        <>
+        { isLoaded 
+            ? <Container sx={{ textAlign: 'right', paddingLeft: '.7rem', paddingRight: '.7rem'}}>
             <Typography
             variant="h2"
-            sx={{ textAlign: 'left', fontSize: '1.5rem'} }
+            sx={{ textAlign: 'left', fontSize: '1.5rem', paddingTop: '.5rem'} }
             >
             Today's Cryptocurrency Prices by Market Cap
             </Typography>
 
             <TableContainer
             component={Paper}
-            sx={{ marginLeft: 'auto', backgroundColor: 'transparent', boxShadow: 0, overflowX: 'visible' }}
+            sx={{ marginLeft: 'auto', backgroundColor: 'transparent', boxShadow: 0 }}
             >
 
                 <Table
@@ -69,14 +73,23 @@ const CoinTable = ()  => {
                     
                     <TableHead sx={{ marginBottom: '1rem', backgroundColor: '#4F4C9E' }}>
                         <TableRow sx={{ borderRadius: '30px' }}>
-                            <TableCell align='center' sx={{ color: 'white', borderBottom: 0 }}>
+                            <TableCell align='center' colSpan={2} sx={{ color: 'white', borderBottom: 0 }}>
                                 Name
                             </TableCell>
-                            <TableCell align='center' sx={{ color: 'white', borderBottom: 0 }}>
+                            <TableCell align='right' sx={{ color: 'white', borderBottom: 0 }}>
                                 Price
                             </TableCell>
-                            <TableCell align='center' sx={{ color: 'white', borderBottom: 0 }}>
+                            <TableCell align='right' sx={{ color: 'white', borderBottom: 0 }}>
                                 24h%
+                            </TableCell>
+                            <TableCell align='right' sx={{ color: 'white', borderBottom: 0 }}>
+                                7d%
+                            </TableCell>
+                            <TableCell align='right' sx={{ color: 'white', borderBottom: 0 }}>
+                                30d%
+                            </TableCell>
+                            <TableCell align='center' sx={{ color: 'white', borderBottom: 0 }}>
+                                Market Cap
                             </TableCell>
                         </TableRow>
                     </TableHead>
@@ -84,20 +97,34 @@ const CoinTable = ()  => {
                     <TableBody sx={{ backgroundColor: '#4F4C9E' }}>
                         {coinData.map((coin: ICoin, i) =>{
                             return (
-                                <TableRow className={`${coin.name} coin`} key={i}>
-                                    <TableCell className='coin-name' align='center' sx={{ borderBottom: 0 }}>
+                                <TableRow className={`${coin.name} coin`} key={i} sx={{overflowX: 'visible'}}>
+                                    <TableCell align='left' sx={{ borderBottom: 0 }}>
                                         <img src={coin.image} alt={coin.name} className={`coin-image ${coin.symbol}`}/>
                                         <span className='market-rank'>{coin.market_cap_rank}</span>
+                                    </TableCell>
+                                    <TableCell className='coin-name' align='left' sx={{ borderBottom: 0 }}>
                                         <p>{coin.name}</p>
                                     </TableCell>
-                                    <TableCell align='center' sx={{ borderBottom: 0 }}>
+                                    <TableCell align='right' sx={{ borderBottom: 0 }}>
                                         <p>${coin.current_price.toLocaleString()}</p>
                                     </TableCell>
-                                    
-                                    <TableCell align='center' sx={{ borderBottom: 0 }}>
+                                    <TableCell align='right' sx={{ borderBottom: 0 }}>
                                         <p><span style={coin.price_change_percentage_24h > 0 ? {color: '#05FF00'} : {color: 'red'}}>
                                             {coin.price_change_percentage_24h.toPrecision(3)}%
                                         </span></p>
+                                    </TableCell>
+                                    <TableCell align='right' sx={{ borderBottom: 0 }}>
+                                        <p><span style={coin.price_change_percentage_7d_in_currency > 0 ? {color: '#05FF00'} : {color: 'red'}}>
+                                            {coin.price_change_percentage_7d_in_currency.toPrecision(3)}%
+                                        </span></p>
+                                    </TableCell>
+                                    <TableCell align='right' sx={{ borderBottom: 0 }}>
+                                        <p><span style={coin.price_change_percentage_30d_in_currency > 0 ? {color: '#05FF00'} : {color: 'red'}}>
+                                            {coin.price_change_percentage_30d_in_currency.toPrecision(3)}%
+                                        </span></p>
+                                    </TableCell>
+                                    <TableCell align='center' sx={{ borderBottom: 0 }}>
+                                        <p>${coin.market_cap.toLocaleString()}</p>
                                     </TableCell>
                                 </TableRow>
                             )
@@ -105,7 +132,12 @@ const CoinTable = ()  => {
                     </TableBody>
                 </Table>
             </TableContainer>
-        </Container>
+            </Container> 
+            : <Box sx={{ width: '100%' }}>
+                <LinearProgress color='inherit' />
+            </Box>
+        }
+        </>
     )
 }
 export default CoinTable;
